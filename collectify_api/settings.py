957be893a75ea04d759f05ac621ae57ce3cf1a91
jgalvani <jchristophegalvani@gmail.com>
2021-12-04 12:20:29 +0100

    fix: replace requirements extension

--- collectify_api/settings.py
+++ collectify_api/settings.py
@@ -18,7 +18,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -161,4 +160,5 @@ WHITENOISE_USE_FINDERS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Activate Django-Heroku.
 django_heroku.settings(locals())
