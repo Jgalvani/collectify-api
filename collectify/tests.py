e03ad24e7e8de609c17b331d986f69936afba849
jgalvani <jchristophegalvani@gmail.com>
2021-12-08 13:51:07 +0100

    feat: tests refactor + CRUD color tests

--- collectify/tests.py
+++ collectify/tests.py
@@ -7,15 +7,137 @@ from rest_framework import status
 from .models import Car, Color, User
 
 # Create your tests here.
+class ColorTest(APITestCase):
+
+    def setUp(self):
+        '''
+        Prepare variables needed by every test.
+        '''
+        # Authenticate.
+        self.user = AuthUser.objects.create_superuser('test_user', '', 'test_password')
+        self.token = Token.objects.create(user=self.user).key
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+
+        # Create color link.
+        self.color_list_endpoint = reverse('color-list')
+
+    # CREATE
+    def test_create_color(self):
+        """
+        Create a color.
+        """
+        # Create a color.
+        data = {'name': 'bleu_test'}
+        response = self.client.post(self.color_list_endpoint, data, format='json')
+
+        # Response status code should be 201.
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # There should be 1 color in the database.
+        self.assertEqual(Color.objects.count(), 1)
+        # Color from database should be "bleu_test".
+        self.assertEqual(Color.objects.first().name, 'bleu_test')
+
+    # RETRIEVE
+    def test_retrieve_color(self):
+        """
+        Retrieve a color.
+        """
+        # Create a color.
+        data = {'name': 'bleu_test'}
+        create_response = self.client.post(self.color_list_endpoint, data, format='json')
+
+        # Retrieve the color.
+        color_detail_endpoint = reverse('color-detail', args=[create_response.data['id']])
+        retrieve_response = self.client.get(color_detail_endpoint, format='json')
+
+        # Response status code should be 200.
+        self.assertEqual(retrieve_response.status_code, status.HTTP_200_OK)
+        # Response data should be a dictionnary.
+        self.assertIsInstance(retrieve_response.data, dict)
+        # Color from response should be "bleu_test".
+        self.assertEqual(retrieve_response.data['name'], 'bleu_test')
+
+    # LIST
+    def test_list_colors(self):
+        """
+        List colors.
+        """
+        # Create colors.
+        colors_data = [
+            {'name': 'bleu_test'},
+            {'name': 'vert_test'},
+            {'name': 'rouge_test'}
+        ]
+        created_colors = []
+        for color_data in colors_data:
+            created_colors.append(self.client.post(self.color_list_endpoint, color_data, format='json').data)
+
+        # List colors.
+        list_response = self.client.get(self.color_list_endpoint, format='json')
+
+        # Response status code should be 200.
+        self.assertEqual(list_response.status_code, status.HTTP_200_OK)
+        # Response data should be a list.
+        self.assertIsInstance(list_response.data, list)
+        # There should be 3 colors in response.
+        self.assertEqual(len(list_response.data), 3)
+        # Colors from response should be the same as created colors.
+        for created_color, color in zip(created_colors, list_response.data):
+            self.assertEqual(created_color['name'], color['name'])
+
+    # UPDATE
+    def test_update_color(self):
+        """
+        Update a color.
+        """
+        # Create a color.
+        data = {'name': 'bleu_test'}
+        create_response = self.client.post(self.color_list_endpoint, data, format='json')
+
+        # Change the name of the color.
+        updated_color = create_response.data
+        updated_color['name'] = 'rouge_test'
+        color_detail_endpoint = reverse('color-detail', args=[updated_color['id']])
+        update_response = self.client.put(color_detail_endpoint, updated_color, format='json')
+
+        # Response status code should be 200.
+        self.assertEqual(update_response.status_code, status.HTTP_200_OK)
+        # Response data should be a dictionnary.
+        self.assertIsInstance(update_response.data, dict)
+        # Color from response should not be "bleu_test".
+        self.assertNotEqual(update_response.data['name'], "bleu_test")
+        # Color from response should be "rouge_test".
+        self.assertEqual(update_response.data['name'], 'rouge_test')
+
+    # DELETE
+    def test_delete_color(self):
+        # Create a color.
+        data = {'name': 'bleu_test'}
+        create_response = self.client.post(self.color_list_endpoint, data, format='json')
+
+        # There should be 1 color in the database.
+        self.assertEqual(Color.objects.count(), 1)
+
+        # Delete a color.
+        color_detail_endpoint = reverse('color-detail', args=[Color.objects.first().id])
+        delete_response = self.client.delete(color_detail_endpoint, format='json')
+
+        # There should not be any color in the database.
+        self.assertEqual(Color.objects.count(), 0)
+
+
 class CarTest(APITestCase):
 
     def setUp(self):
         '''
         Prepare variables needed by every test.
         '''
+        # Authenticate.
         self.user = AuthUser.objects.create_superuser('test_user', '', 'test_password')
         self.token = Token.objects.create(user=self.user).key
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+
+        # Create links.
         self.car_list_endpoint = reverse('car-list')
         self.color_list_endpoint = reverse('color-list')
 
@@ -28,11 +150,16 @@ class CarTest(APITestCase):
         data = {'name': 'Tesla_test', 'colors': []}
         response = self.client.post(self.car_list_endpoint, data, format='json')
 
+        # Response status code should be 201.
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # There should be 1 car in the database.
         self.assertEqual(Car.objects.count(), 1)
-        self.assertEqual(Car.objects.get().name, 'Tesla_test')
-        self.assertEqual(Car.objects.get().colors.count(), 0)
-        self.assertEqual(list(Car.objects.get().colors.all()), [])
+        # Car from database should be "Tesla_test".
+        self.assertEqual(Car.objects.first().name, 'Tesla_test')
+        # Car should not have any color in the database.
+        self.assertEqual(Car.objects.first().colors.count(), 0)
+        # Attribute colors from car should be an empty list.
+        self.assertEqual(list(Car.objects.first().colors.all()), [])
 
 
     def test_create_car_with_one_color(self):
@@ -47,15 +174,16 @@ class CarTest(APITestCase):
         car_data = {'name': 'Tesla_test', 'colors': [color_response.data]}
         car_response = self.client.post(self.car_list_endpoint, car_data, format='json')
 
-        # Preparing to compare the color created and the color of the car.
-        car_color_name = Car.objects.get().colors.get().name
-        created_color_name = color_data['name']
-
+        # Response status code should be 201.
         self.assertEqual(car_response.status_code, status.HTTP_201_CREATED)
+        # There should be 1 car in the database.
         self.assertEqual(Car.objects.count(), 1)
-        self.assertEqual(Car.objects.get().name, 'Tesla_test')
-        self.assertEqual(Car.objects.get().colors.count(), 1)
-        self.assertEqual(car_color_name, created_color_name)
+        # Car from database should be "Tesla_test".
+        self.assertEqual(Car.objects.first().name, 'Tesla_test')
+        # Car should have 1 color in the database.
+        self.assertEqual(Car.objects.first().colors.count(), 1)
+        # Color from car should be bleu_test.
+        self.assertEqual(Car.objects.first().colors.first().name, 'bleu_test')
 
     def test_create_car_with_colors(self):
         """
@@ -76,11 +204,16 @@ class CarTest(APITestCase):
         car_data = {'name': 'Tesla_test', 'colors': created_colors}
         car_response = self.client.post(self.car_list_endpoint, car_data, format='json')
 
+        # Response status code should be 201.
         self.assertEqual(car_response.status_code, status.HTTP_201_CREATED)
+        # There should be 1 car in the database.
         self.assertEqual(Car.objects.count(), 1)
-        self.assertEqual(Car.objects.get().name, 'Tesla_test')
-        self.assertEqual(Car.objects.get().colors.count(), 3)
-        for created_color, car_color in zip(created_colors, Car.objects.get().colors.all()):
+        # Car from database should be "Tesla_test".
+        self.assertEqual(Car.objects.first().name, 'Tesla_test')
+        # Car should have 3 colors in the database.
+        self.assertEqual(Car.objects.first().colors.count(), 3)
+        # Colors from car should be the same as created colors.
+        for created_color, car_color in zip(created_colors, Car.objects.first().colors.all()):
             self.assertEqual(created_color['name'], car_color.name)
 
     # RETRIEVE
@@ -93,13 +226,16 @@ class CarTest(APITestCase):
         create_response = self.client.post(self.car_list_endpoint, data, format='json')
 
         # Retrieve the car.
-        car_id = create_response.data['id']
-        car_detail_endpoint = reverse('car-detail', args=[car_id])
+        car_detail_endpoint = reverse('car-detail', args=[create_response.data['id']])
         retrieve_response = self.client.get(car_detail_endpoint, format='json')
 
+        # Response status code should be 200.
         self.assertEqual(retrieve_response.status_code, status.HTTP_200_OK)
+        # Response data should be a dictionnary.
         self.assertIsInstance(retrieve_response.data, dict)
-        self.assertEqual(retrieve_response.data['name'], data['name'])
+        # Car from response should be "Tesla_test".
+        self.assertEqual(retrieve_response.data['name'], 'Tesla_test')
+        # Attribute colors from car should be an empty list.
         self.assertEqual(retrieve_response.data['colors'], [])
 
     def test_retrieve_car_with_one_color(self):
@@ -115,18 +251,17 @@ class CarTest(APITestCase):
         create_response = self.client.post(self.car_list_endpoint, data, format='json')
 
         # Retrieve the car.
-        car_id = create_response.data['id']
-        car_detail_endpoint = reverse('car-detail', args=[car_id])
+        car_detail_endpoint = reverse('car-detail', args=[create_response.data['id']])
         retrieve_response = self.client.get(car_detail_endpoint, format='json')
 
-        # Preparing to compare the color created and the color of the car.
-        car_color_name = retrieve_response.data['colors'][0]['name']
-        created_color_name = color_data['name']
-
+        # Response status code should be 200.
         self.assertEqual(retrieve_response.status_code, status.HTTP_200_OK)
+        # Response data should be a dictionnary.
         self.assertIsInstance(retrieve_response.data, dict)
-        self.assertEqual(retrieve_response.data['name'], data['name'])
-        self.assertEqual(car_color_name, created_color_name)
+        # Car from response should be "Tesla_test".
+        self.assertEqual(retrieve_response.data['name'], 'Tesla_test')
+        # Color from car should be bleu_test.
+        self.assertEqual(retrieve_response.data['colors'][0]['name'], 'bleu_test')
 
     def test_retrieve_car_with_colors(self):
         """
@@ -142,19 +277,21 @@ class CarTest(APITestCase):
         for color_data in colors_data:
             created_colors.append(self.client.post(self.color_list_endpoint, color_data, format='json').data)
 
-
         # Create a car.
         data = {'name': 'Tesla_test', 'colors': colors_data}
         create_response = self.client.post(self.car_list_endpoint, data, format='json')
 
         # Retrieve the car.
-        car_id = create_response.data['id']
-        car_detail_endpoint = reverse('car-detail', args=[car_id])
+        car_detail_endpoint = reverse('car-detail', args=[create_response.data['id']])
         retrieve_response = self.client.get(car_detail_endpoint, format='json')
 
+        # Response status code should be 200.
         self.assertEqual(retrieve_response.status_code, status.HTTP_200_OK)
+        # Response data should be a dictionnary.
         self.assertIsInstance(retrieve_response.data, dict)
-        self.assertEqual(retrieve_response.data['name'], data['name'])
+        # Car from response should be "Tesla_test".
+        self.assertEqual(retrieve_response.data['name'], 'Tesla_test')
+        # Colors from car should be the same as created colors.
         for created_color, car_color in zip(created_colors, retrieve_response.data['colors']):
             self.assertEqual(created_color, car_color)
 
@@ -186,9 +323,13 @@ class CarTest(APITestCase):
         # List cars.
         list_response = self.client.get(self.car_list_endpoint, format='json')
 
+        # Response status code should be 200.
         self.assertEqual(list_response.status_code, status.HTTP_200_OK)
+        # Response data should be a list.
         self.assertIsInstance(list_response.data, list)
+        # There should be 3 cars in response.
         self.assertEqual(len(list_response.data), 3)
+        # Cars from response should be the same as created cars.
         for created_car, car in zip(created_cars, list_response.data):
             self.assertEqual(created_car['name'], car['name'])
             self.assertEqual(created_car['colors'], car['colors'])
@@ -212,19 +353,21 @@ class CarTest(APITestCase):
         data = {'name': 'Tesla_test', 'colors': colors_data}
         create_response = self.client.post(self.car_list_endpoint, data, format='json')
 
-        # Retrieve the car.
-        car_id = create_response.data['id']
-        car_detail_endpoint = reverse('car-detail', args=[car_id])
-        retrieve_response = self.client.get(car_detail_endpoint, format='json')
-
         # Change the name of the car.
-        retrieve_response.data['name'] = 'BMW_test'
-        update_response = self.client.put(car_detail_endpoint, retrieve_response.data, format='json')
+        updated_car = create_response.data
+        updated_car['name'] = 'BMW_test'
+        car_detail_endpoint = reverse('car-detail', args=[updated_car['id']])
+        update_response = self.client.put(car_detail_endpoint, updated_car, format='json')
 
+        # Response status code should be 200.
         self.assertEqual(update_response.status_code, status.HTTP_200_OK)
+        # Response data should be a dictionnary.
         self.assertIsInstance(update_response.data, dict)
-        self.assertEqual(update_response.data['name'], retrieve_response.data['name'])
-        self.assertNotEqual(update_response.data['name'], data['name'])
+        # Car from update response should be "BMW_test".
+        self.assertEqual(update_response.data['name'], 'BMW_test')
+        # Car from update response should not be "Tesla_test".
+        self.assertNotEqual(update_response.data['name'], 'Tesla_test')
+        # Colors from update response should be the same as created colors.
         self.assertEqual(update_response.data['colors'], created_colors)
 
     def test_update_car_colors(self):
@@ -245,28 +388,32 @@ class CarTest(APITestCase):
         data = {'name': 'Tesla_test', 'colors': colors_data}
         create_response = self.client.post(self.car_list_endpoint, data, format='json')
 
-        # Retrieve the car.
-        car_id = create_response.data['id']
-        car_detail_endpoint = reverse('car-detail', args=[car_id])
-        retrieve_response = self.client.get(car_detail_endpoint, format='json')
-
         # Delete the colors of the car.
-        retrieve_colors = retrieve_response.data['colors']
-        retrieve_response.data['colors'] = []
-        delete_colors_response = self.client.put(car_detail_endpoint, retrieve_response.data, format='json')
+        updated_car = create_response.data
+        updated_car['colors'] = []
+        car_detail_endpoint = reverse('car-detail', args=[updated_car['id']])
+        delete_colors_response = self.client.put(car_detail_endpoint, updated_car, format='json')
 
+        # Response status code should be 200.
         self.assertEqual(delete_colors_response.status_code, status.HTTP_200_OK)
+        # Response data should be a dictionnary.
         self.assertIsInstance(delete_colors_response.data, dict)
-        self.assertEqual(delete_colors_response.data['name'], retrieve_response.data['name'])
-        self.assertEqual(delete_colors_response.data['name'], data['name'])
+        # Car from update response should be "Tesla_test".
+        self.assertEqual(delete_colors_response.data['name'], 'Tesla_test')
+        # Colors from car should not be the same as created colors.
+        self.assertNotEqual(delete_colors_response.data['colors'], created_colors)
+        # The attribute colors from car should be an empty list.
         self.assertEqual(delete_colors_response.data['colors'], [])
-        self.assertNotEqual(delete_colors_response.data['colors'], retrieve_colors)
 
         # Add new colors to the car.
-        delete_colors_response.data['colors'] = created_colors[1:3]
+        partial_created_colors = created_colors[1:3]
+        delete_colors_response.data['colors'] = partial_created_colors
+        car_detail_endpoint = reverse('car-detail', args=[delete_colors_response.data['id']])
         add_colors_response = self.client.put(car_detail_endpoint, delete_colors_response.data, format='json')
 
-        self.assertEqual(add_colors_response.data['colors'], created_colors[1:3])
+        # Colors from car should be the same as partial created colors.
+        self.assertEqual(add_colors_response.data['colors'], partial_created_colors)
+        # The attribute colors from car should not be an empty list.
         self.assertNotEqual(add_colors_response.data['colors'], [])
 
     # DELETE
@@ -275,11 +422,12 @@ class CarTest(APITestCase):
         data = {'name': 'Tesla_test', 'colors': []}
         create_response = self.client.post(self.car_list_endpoint, data, format='json')
 
+        # There should be 1 car in the database.
         self.assertEqual(Car.objects.count(), 1)
 
         # Delete a car.
-        car_id = Car.objects.get().id
-        car_detail_endpoint = reverse('car-detail', args=[car_id])
+        car_detail_endpoint = reverse('car-detail', args=[Car.objects.first().id])
         delete_response = self.client.delete(car_detail_endpoint, format='json')
 
+        # There should not be any car in the database.
         self.assertEqual(Car.objects.count(), 0)
