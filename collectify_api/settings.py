3fc83fbb730fcbedf638f1627ad9434aee59a444
jgalvani <jchristophegalvani@gmail.com>
2021-12-13 13:54:02 +0100

    fix: add localhost to CORS

--- collectify_api/settings.py
+++ collectify_api/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:4200",
+    'http://localhost:4200',
     "https://collectify-ui.herokuapp.com",
     "https://collectify-admin.herokuapp.com",
 ]
